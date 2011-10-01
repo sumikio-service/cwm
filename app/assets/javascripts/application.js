4262cae8dce82c5e787e3d43db02e4d63e030948
Sumiki <sumikio@gmail.com>
2011-10-01 00:19:05 -0700

    first demo at homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,32 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(function(){
+
+    $('#gm_marketing_branding')
+    .bind('mouseover', function(){
+        $('#gm_sub_complete_brand_experience').slideDown();
+    })
+
+    $('#gm_about')
+    .bind('mouseover', function(){
+        $('#gm_sub_clients').slideDown();
+    })
+
+    $('.gm_main_menu').bind('mouseover', function(attr){
+        var mouse_over_id = $(attr.target).parent().attr('id')
+        if( mouse_over_id != 'gm_marketing_branding' ){
+            $('#gm_sub_complete_brand_experience').slideUp();
+        }
+        if( mouse_over_id != 'gm_about' ){
+            $('#gm_sub_clients').slideUp();
+        }
+    })
+    
+    $('ul#gm').bind('mouseleave', function(attr){
+        $('#gm_sub_complete_brand_experience').slideUp();
+        $('#gm_sub_clients').slideUp();
+    })
+    
+})
